2f5689f468d7efd82c11349f1c6e172f70ca038c
Rebecca Johnson <beccanjohnson@gmail.com>
2020-12-07 20:41:18 -0600

    finsihed studio

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 window.addEventListener("load", function() {
     fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
         response.json().then(function(json) {
-            const container = document.getElementById("container");
+            let container = document.getElementById("container");
             for (let i = 0; i<json.length; i++) {
-                container.append(`
-                    <div class="id">
+                container.innerHTML = container.innerHTML + `
+                    <div class="astronaut">
                         <div class="bio">            
                             <h3> ${json[i].firstName} ${json[i].lastName} </h3>
                             <ul>
                                 <li>Hours in space: ${json[i].hoursInSpace}</li>
-                                <li>Active: ${json[i].active}</li>
+                                <li id="active">Active: ${json[i].active}</li>
                                 <li>Skills: ${json[i].skills}</li>
                             </ul>
                         </div>
-                        <img class="avatar" src="images/${json[i].picture}"
+                        <img class="avatar" src="${json[i].picture}"
                     </div>
-                    `);
+                    `;
             } 
         });
     });
